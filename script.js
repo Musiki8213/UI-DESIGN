21725d524c2946965cb718ca959bbcca4456752a
Musiki8213 <sithomolamusiki@gmail.com>
2025-07-06 16:41:54 +0200

    changed css and html and added js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function moreInfo(){
+    const link = "https://youtu.be/byN1Juy-fpI";
+
+    document.getElementById("message").innerHTML = `<a href="${link} target="_blank">${link}</a>`;
+}
+ 
